25097348f0f61e43fa96799182a0553cef344a59
KojiTashiro <koji.tashiro222@gmail.com>
2019-02-13 09:53:57 +0900

    年齢と性別で識別し、指定したデータを表示する機能を実装しました。

--- home.blade.php
+++ home.blade.php
@@ -7,7 +7,30 @@
         こんにちは！
         @if (Auth::check())
             {{\Auth::user()->name}}さん<br>
-            <a href="/auth/logout">ログアウト</a>
+            <a href="/auth/logout">ログアウト</a><br>
+            @foreach ($items as $item)
+                <li>{{$item->rice_products_id}}
+                <li>{{$item->bread_products_id}}
+                <li>{{$item->noodles_products_id}}
+                <li>{{$item->meat_products_id}}
+                <li>{{$item->seefood_products_id}}
+                <li>{{$item->vagetable_products_id}}
+                <li>{{$item->fruit_products_id}}
+                <li>{{$item->egg_products_id}}
+                <li>{{$item->dairy_products_id}}
+                <li>{{$item->dessert_products_id}}
+            @endforeach
+            <br>
+            <table>
+                <tr>
+                    <th>あなたが1日で必要な栄養素</th>
+                </tr>
+                <tr>
+                    @foreach ($required_data as $data)
+                        <td>{{$data->getData()}}</td>
+                    @endforeach
+                </tr>
+            </table>
         @else
             ゲストさん<br>
             <a href="/auth/login">ログイン</a>
