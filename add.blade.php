b9ff3f8a677e141b897a8bc75a4866a376139823
KojiTashiro <koji.tashiro222@gmail.com>
2019-03-05 18:16:21 +0900

    選択する食べ物を追加しました

--- add.blade.php
+++ add.blade.php
@@ -13,6 +13,13 @@
             <a href="/auth/login">ログイン</a>
             <a href="/auth/register">会員登録</a>
         @endif
+        @if ($errors->any())
+            <ul>
+                @foreach ($errors->all() as $error)
+                    <li>{{$error}}</li>
+                @endforeach
+            </ul>
+        @endif
         <form action="/home" method="post">
             {{ csrf_field() }}
             <table>
@@ -20,40 +27,101 @@
                     @foreach ($Rice as $rice)
                         @foreach ($Curry as $curry)
                     <tr>
-                        <td>{{$cotegory->rice_dishes}}</td>
+                        <td>米類({{$cotegory->rice_dishes}})</td>
                         <td>
-                            <select name="food">    
-                                <option value="{{$rice}}">{{$rice->food_name}}</option>
-                                <option value="{{$curry}}">{{$curry->food_name}}</option>
+                            <input type="radio" name="food[]" value="{{$rice}}">ライス
+                            <input type="radio" name="food[]" value="{{$curry}}">カレーライス
+                        </td>
+                    </tr>
+                    <tr>
+                        <td>パン類({{$cotegory->bread_products}})</td>
+                        <td>
+                            <select name="food[]">
+                                <option value="">--</option>  
+                                <option value=""></option>
+                                <option value="">カレーパン</option>
                             </select>
                         </td>
                     </tr>
                     <tr>
-                        <td>{{$cotegory->bread_products}}</td>
+                        <td>麺類({{$cotegory->noodle_dishes}})</td>
+                        <td>
+                            <input type="radio" name="food[]" value="{{$SoySourceRamen}}">醤油ラーメン
+                        </td>
                     </tr>
                     <tr>
-                        <td>{{$cotegory->noodle_dishes}}</td>
+                        <td>肉類({{$cotegory->meat_dishesd}})</td>
+                        <td>
+                            <select name="food[]">
+                                <option value="">--</option>   
+                                <option value=""></option>
+                                <option value="">カレーパン</option>
+                            </select>
+                        </td>
                     </tr>
                     <tr>
-                        <td>{{$cotegory->meat_dishesd}}</td>
+                        <td>海鮮類({{$cotegory->seefood_dishes}})</td>
+                        <td>
+                            <select name="food[]">
+                                <option value="">--</option>    
+                                <option value=""></option>
+                                <option value="">マグロ刺身</option>
+                            </select>
+                        </td>
+                    </td>
                     </tr>
                     <tr>
-                        <td>{{$cotegory->seefood_dishes}}</td>
+                        <td>野菜類({{$cotegory->vagetable_dishes}})</td>
+                        <td>
+                            <select name="food[]">
+                                <option value="">--</option>    
+                                <option value=""></option>
+                                <option value="">野菜スティック</option>
+                            </select>
+                        </td>
+                    </td>
                     </tr>
                     <tr>
-                        <td>{{$cotegory->vagetable_dishes}}</td>
+                        <td>果物類({{$cotegory->fruits}})</td>
+                        <td>
+                            <select name="food[]">
+                                <option value="">--</option>    
+                                <option value=""></option>
+                                <option value="">パイナップル</option>
+                            </select>
+                        </td>
+                    </td>
                     </tr>
                     <tr>
-                        <td>{{$cotegory->fruits}}</td>
+                        <td>卵類({{$cotegory->egg_dishes}})</td>
+                        <td>
+                            <select name="food[]">
+                                <option value="">--</option>    
+                                <option value=""></option>
+                                <option value="">目玉焼き</option>
+                            </select>
+                        </td>
+                    </td>
                     </tr>
                     <tr>
-                        <td>{{$cotegory->egg_dishes}}</td>
+                        <td>乳製品類({{$cotegory->dairy_products}})</td>
+                        <td>
+                            <select name="food[]">
+                                <option value="">--</option>    
+                                <option value=""></option>
+                                <option value="">ヤクルト</option>
+                            </select>
+                        </td>
                     </tr>
                     <tr>
-                        <td>{{$cotegory->dairy_products}}</td>
-                    </tr>
-                    <tr>
-                        <td>{{$cotegory->desserts}}</td>
+                        <td>デザート類({{$cotegory->desserts}})</td>
+                        <td>
+                            <select name="food[]">
+                                <option value="">--</option>    
+                                <option value=""></option>
+                                <option value="">ゼリー</option>
+                            </select>
+                        </td>
                     </tr>
                         @endforeach
                     @endforeach
